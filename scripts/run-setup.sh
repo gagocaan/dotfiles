ddf428a65364a2b95e41add1dd285a7a34da758b
gagocaan <gagocaan@gmail.com>
2022-01-18 12:27:12 -0500

    Update automatica setup * Implement case for many OS * Remove linux setup script

---
+++ scripts/run-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# TODO: Improve for other OS
+case $(uname) in
+Darwin)
+    echo "Install requirements for Mac OS ..."
+    which -s brew
+    if [[ $? != 0 ]]; then
+        # Install Homebrew
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew update
+    brew install coreutils git-delta git-flow go lolcat neovim poetry powerlevel10k tree virtualenv zsh zsh-autosuggestions zsh-syntax-highlighting
+    echo "Installation finished."
+    ;;
+Linux)
+    echo "Install requirements for Linux ..."
+    apt update
+    apt install -y coreutils git delta git-flow neovim curl tree lolcat virtualenv zsh zsh-autosuggestions zsh-syntax-highlighting
+    echo "Installation finished."
+    ;;
+*)
+    echo "The Operating System is not recognized."
+    ;;
+esac
