dd619705bed4efd4f44c6710b74c7f0d6855913b
gagocaan <gagocaan@gmail.com>
2022-05-03 21:21:29 -0500

    Fix install in Raspbian

---
+++ scripts/install-yarn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+sudo apt remove cmdtest -y
+sudo apt remove yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install yarn -y
+yarn install
