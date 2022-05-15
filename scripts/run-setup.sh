2c727ff29ab82a5720a4c33981f83fab7d12f721
gagocaan <gagocaan@gmail.com>
2022-05-15 12:06:27 -0500

    Update install to Linux OS

--- scripts/run-setup.sh
+++ scripts/run-setup.sh
@@ -1,51 +1,26 @@
 #!/bin/bash -e
 
-case $(uname) in
-Darwin)
-    echo "Install requirements for Mac OS ..."
-    which -s brew
-    if [[ $? != 0 ]]; then
-        # Install Homebrew
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    brew upgrade
-    brew cleanup
-    brew install coreutils git-delta git-flow go lolcat neovim node poetry powerlevel10k tree virtualenv zsh zsh-autosuggestions zsh-syntax-highlighting
-    pip3 install pynvim black flake8 isort
-    echo "Installation finished."
-    ;;
-Linux)
-    if [[ $(
-        . /etc/os-release
-        echo "$NAME"
-    ) == "Fedora Linux" ]]; then
-        echo "Install requirements for Fedora Linux"
-        dnf update
-        dnf install -y coreutils curl git neovim nodejs python3-pip tree lolcat virtualenv zsh zsh-autosuggestions zsh-syntax-highlighting
-        # TODO: Test if has pip
-        pip3 install pynvim black flake8 isort
-        echo "Installation finished"
-    elif [[ $(
-        . /etc/os-release
-        echo "$NAME"
-    ) == "Alpine Linux" ]]; then
-        echo "Install requirements for Alpine Linux"
-        apk update
-        apk add coreutils curl git git-flow neovim python3 py3-pip tree zsh zsh-autosuggestions zsh-syntax-highlighting
-        # TODO: Test if has pip
-        pip3 install pynvim black flake8 isort
-    else
-        echo "Install requirements for Linux $NAME"
-        sudo apt update
-        sudo apt install -y coreutils git git-flow git-core neovim nodejs fonts-powerline python3-pip curl tree lolcat virtualenv zsh zsh-autosuggestions zsh-syntax-highlighting
-        # TODO: Test if has pip
-        pip3 install pynvim black flake8 isort
+if [[ $(
+    . /etc/os-release
+    echo "$NAME"
+) == "Fedora Linux" ]]; then
+    echo "Install requirements for Fedora Linux"
+    dnf update
+    dnf install -y coreutils curl git neovim nodejs tree lolcat virtualenv zsh
+elif [[ $(
+    . /etc/os-release
+    echo "$NAME"
+) == "Alpine Linux" ]]; then
+    echo "Install requirements for Alpine Linux"
+    apk update
+    apk add coreutils curl git git-flow neovim nodejs python3 tree yarn zsh
+else
+    echo "Install requirements for Linux $NAME"
+    sudo apt update
+    sudo apt install -y coreutils git git-flow git-core neovim nodejs fonts-powerline curl tree lolcat virtualenv zsh
+fi
 
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        echo "Installation finished."
-    fi
-    ;;
-*)
-    echo "The Operating System is not recognized."
-    ;;
-esac
+python3 -m ensurepip
+pip3 install pynvim black flake8 isort
+
+echo "Installation finished"
