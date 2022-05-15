55e30e5788e4f54de6abee3f43d292ab23b06ceb
gagocaan <gagocaan@gmail.com>
2022-05-15 12:45:05 -0500

    :fire: Fix Linux setup

--- scripts/run-setup.sh
+++ scripts/run-setup.sh
@@ -14,10 +14,17 @@ elif [[ $(
     echo "Install requirements for Alpine Linux"
     apk update
     apk add coreutils curl git git-flow neovim nodejs python3 tree yarn zsh
+elif [[ $(
+    . /etc/os-release
+    echo "$NAME"
+) == "Raspbian GNU/Linux" ]]; then
+    echo "No actions to perform"
 else
+    . /etc/os-release
     echo "Install requirements for Linux $NAME"
     sudo apt update
     sudo apt install -y coreutils git git-flow git-core neovim nodejs fonts-powerline curl tree lolcat virtualenv zsh
+    sudo apt autoremove
 fi
 
 python3 -m ensurepip
