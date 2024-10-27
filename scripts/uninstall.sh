ff4c4c2b9923f116fbdfd1bb3f228c178c126e23
gagocaan <gagocaan@gmail.com>
2024-10-27 11:30:23 -0500

    Replace poetry with uv

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-# Remove Python libraries
-pip3 freeze --exclude wheel >requirements.txt # Backup Python dependencies
-pip3 freeze --exclude wheel | xargs pip3 uninstall -y --break-system-packages
-
-pyenv shell system  # Switch to system python
-pyenv global system # Switch to system python
-PYENV_ROOT=$(pyenv root)
-source $HOME/.zshrc # Reload the source
-
-# Remove Poetry
-pipx uninstall poetry
-
 # Remove Brew formulas
 brew bundle dump --force # Backup formulas
 brew tap >Brewtap        # Taps (Third-Party Repositories)
@@ -27,7 +15,3 @@ sed -i '' 's/\(^export PYENV_ROOT.*\)/#\1/' $HOME/.dotfiles/zshrc
 sed -i '' 's/\(^command -v pyenv .*$\)/#\1/' $HOME/.dotfiles/zshrc
 sed -i '' 's/\(^eval "$(pyenv init -)"$\)/#\1/' $HOME/.dotfiles/zshrc
 sed -i '' 's/\(^alias brew=.*\)/#\1/' $HOME/.dotfiles/zshrc
-
-# Remove Pyenv
-rm -rf $PYENV_ROOT
-rm -rf ~/.pyenv
--- zshrc
+++ zshrc
@@ -3,15 +3,6 @@ source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-
-# Poetry
-export PATH="$HOME/.local/bin:$PATH"
-
 # Brew
 export PATH="/usr/local/sbin:$PATH"
 
